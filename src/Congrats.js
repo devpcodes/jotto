00d34e7c0db4e1b0eab232b758d70ce7e618ac95
devpcodes <43083900+devpcodes@users.noreply.github.com>
2018-12-01 22:14:21 +0800

    init render
    初始化測試，props true和false時的結果

--- src/Congrats.js
+++ src/Congrats.js
@@ -1,9 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-export default () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+export default props => {
+    if (props.success) {
+        return (
+            <div data-test="component-congrats">
+                <span data-test="congrats-message">
+                    Congratulations! You guessed the word!
+                </span>
+            </div>
+        );
+    } else {
+        return <div data-test="component-congrats" />;
+    }
+};
--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -2,16 +2,29 @@ import React from "react";
 import Enzyme, { shallow } from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
 
+import { findByTestAttr } from "../test/testUtils.js";
 import Congrats from "./Congrats";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 const setup = (props = {}) => {
-  return shallow(<Congrats {...props} />);
+    return shallow(<Congrats {...props} />);
 };
 
-it("renders without errors", () => {});
+it("renders without errors", () => {
+    const wrapper = setup();
+    const component = findByTestAttr(wrapper, "component-congrats");
+    expect(component.length).toBe(1);
+});
 
-it("renders no text when success props is false", () => {});
+it("renders no text when success props is false", () => {
+    const wrapper = setup({ success: false });
+    const component = findByTestAttr(wrapper, "component-congrats");
+    expect(component.text()).toBe("");
+});
 
-it("renders non-empty congrats message when success prop is true", () => {});
+it("renders non-empty congrats message when success prop is true", () => {
+    const wrapper = setup({ success: true });
+    const message = findByTestAttr(wrapper, "congrats-message");
+    expect(message.text().length).not.toBe(0);
+});
