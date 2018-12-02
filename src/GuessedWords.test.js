b52a8a21b9ee5c94f56f8f7d10ed2dcdad21695d
devpcodes <43083900+devpcodes@users.noreply.github.com>
2018-12-02 12:19:20 +0800

    props check (#3)
    物件在陣列裡的規格驗證

---
+++ src/GuessedWords.test.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { findByTestAttr, checkProps } from "../test/testUtils";
+import GuessedWords from "./GuessedWords";
+
+const defaultProps = {
+    guessedWords: [{ guessedWord: "train", letterMatchCount: 3 }]
+};
+
+const setup = (props = {}) => {
+    const setupProps = { ...defaultProps, ...props };
+    return shallow(<GuessedWords {...setupProps} />);
+};
+
+it("does not throw warning with expected props", () => {
+    checkProps(GuessedWords, defaultProps);
+});
+
+describe("it there are no words guessed", () => {});
+
+describe("if there are words guessed", () => {});
