12e2d3bc255cbef438391b5a572c4e03814b9d49
devpcodes <43083900+devpcodes@users.noreply.github.com>
2018-12-02 11:24:54 +0800

    Csb 1543720763108 (#2)
    * init render
    
    初始化測試，props true和false時的結果
    
    * check propTypes
    
    檢測propTypes
    
    * 測試設定
    
    設置測試設定檔

--- src/Congrats.js
+++ src/Congrats.js
@@ -1,9 +1,22 @@
-import React, { Component } from 'react';
+import React from "react";
+import PropTypes from "prop-types";
 
-export default () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const Congrats = props => {
+    if (props.success) {
+        return (
+            <div data-test="component-congrats">
+                <span data-test="congrats-message">
+                    Congratulations! You guessed the word!
+                </span>
+            </div>
+        );
+    } else {
+        return <div data-test="component-congrats" />;
+    }
+};
+
+Congrats.propTypes = {
+    success: PropTypes.bool.isRequired
+};
+
+export default Congrats;
