e85883d41ae1a3a9546d3dbf027d6d302a369af6
devpcodes <43083900+devpcodes@users.noreply.github.com>
2018-12-03 21:15:12 +0800

    App.js
    最大的入口組件和測試

--- src/index.js
+++ src/index.js
@@ -2,22 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 
-import "./styles.css";
-import GuessedWords from "./GuessedWords";
-import Congrats from "./Congrats";
+
+import App from './App';
 import store from "./configureStore";
 
-function App() {
-    return (
-        <div className="container">
-            <h1>Jotto</h1>
-            <Congrats success={true} />
-            <GuessedWords
-                guessedWords={[{ guessedWord: "train", letterMatchCount: 3 }]}
-            />
-        </div>
-    );
-}
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
